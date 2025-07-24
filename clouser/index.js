4f3b39026b32aaefa007a1165697a953e702fdad
rohitlkr7 <128993662+rohitlkr7@users.noreply.github.com>
2025-07-24 12:31:08 +0530

    add & update

---
+++ clouser/index.js
@@ -0,0 +1,17 @@
+// function which whenever called print a
+// count and if it's called afterwards it
+// updates the count by +1
+
+
+let count = 0 
+function counter(){
+   count++;
+
+   console.log("count is " + count);
+}
+
+counter();
+counter();
+counter();
+counter();
+
---
+++ modu/index.js
@@ -0,0 +1,2 @@
+import add from './modu/add.js';
+console.log(add(4,8));
