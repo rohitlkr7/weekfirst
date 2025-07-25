f985c09da193b64ab893d7e10030c154ce005f7d
rohitlkr7 <128993662+rohitlkr7@users.noreply.github.com>
2025-07-25 21:35:08 +0530

    file updated

---
+++ HOF/index.js
@@ -0,0 +1,16 @@
+arr = ["apple","banana", "cherry"];
+
+function searchInput(fruits){
+
+}
+
+
+// reduce 
+
+arr = [2,3,4,5,6,7,8,9,10];
+
+let newarr = arr.reduce((acc,curr)=>{
+    return acc +curr;
+},10)
+
+console.log(newarr);
